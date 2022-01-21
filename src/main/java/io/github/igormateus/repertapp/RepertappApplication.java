be347363c63e84a417d6fe5a4153f8642662fbc7
igormateus <igor_matteus@yahoo.com.br>
2022-01-20 23:13:38 -0300

    add spring boot stater security and disable autoconfiguration

--- src/main/java/io/github/igormateus/repertapp/RepertappApplication.java
+++ src/main/java/io/github/igormateus/repertapp/RepertappApplication.java
@@ -2,8 +2,9 @@ package io.github.igormateus.repertapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class RepertappApplication {
 
 	public static void main(String[] args) {
