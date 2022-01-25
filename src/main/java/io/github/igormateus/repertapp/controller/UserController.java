293b69ca14e5dc86f351d9d3e813bf0acff2b47e
igormateus <igor_matteus@yahoo.com.br>
2022-01-25 20:10:50 -0300

    refact functions signin and signup to AuthController

--- src/main/java/io/github/igormateus/repertapp/controller/UserController.java
+++ src/main/java/io/github/igormateus/repertapp/controller/UserController.java
@@ -1,18 +1,17 @@
 package io.github.igormateus.repertapp.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.github.igormateus.repertapp.dto.UserAuthDTO;
-import io.github.igormateus.repertapp.dto.UserDataDTO;
-import io.github.igormateus.repertapp.dto.UserResponseDTO;
-import io.github.igormateus.repertapp.model.AppUser;
+import io.github.igormateus.repertapp.dto.user.UserResponseDTO;
+import io.github.igormateus.repertapp.dto.user.UserUpdateDTO;
 import io.github.igormateus.repertapp.service.UserService;
 import lombok.RequiredArgsConstructor;
 
@@ -23,22 +22,16 @@ public class UserController {
 
     private final UserService userService;
     private final ModelMapper modelMapper;
-
+    
     // Returns current user's data
     @GetMapping("/me")
     public UserResponseDTO whoami(HttpServletRequest req) {
         return modelMapper.map(userService.whoami(req), UserResponseDTO.class);
     }
 
-    // Creates user and returns its JWT token
-    @PostMapping("/signup")
-    public String signup(@RequestBody UserDataDTO user) {
-        return userService.signup(modelMapper.map(user, AppUser.class));
-    }
-
-    // Authenticates user and returns its JWT token.
-    @PostMapping("/signin")
-    public String login(@RequestBody UserAuthDTO user) {
-        return userService.signin(user.getUsername(), user.getPassword());
+    // Edits user's data
+    @PutMapping("/me")
+    public UserResponseDTO editUser(@Valid @RequestBody UserUpdateDTO user, HttpServletRequest req) {
+        return modelMapper.map(userService.edit(user, req), UserResponseDTO.class);
     }
 }
