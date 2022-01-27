36e64744db6959d3f9ddac3d67b2064bdd317460
igormateus <igor_matteus@yahoo.com.br>
2022-01-27 13:45:42 -0300

    create function valideUpdate

--- src/main/java/io/github/igormateus/repertapp/validation/BandValidation.java
+++ src/main/java/io/github/igormateus/repertapp/validation/BandValidation.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
+import io.github.igormateus.repertapp.dto.band.BandUpdateDTO;
 import io.github.igormateus.repertapp.exception.CustomException;
 import io.github.igormateus.repertapp.model.Band;
 import io.github.igormateus.repertapp.repository.BandRepository;
@@ -25,5 +26,13 @@ public class BandValidation {
 
         if (errors.size() > 0)
             throw new CustomException(String.join("; ", errors), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    public void valideUpdate(Band bandSaved, BandUpdateDTO bandUpdate) {
+        List<String> errors = new ArrayList<String>();
+
+        if (!bandSaved.getName().equals(bandUpdate.getName()) 
+                && bandRepository.existsByName(bandUpdate.getName()))
+            errors.add(String.format("Username '%s' is already in use", bandUpdate.getName()));
     }
 }
