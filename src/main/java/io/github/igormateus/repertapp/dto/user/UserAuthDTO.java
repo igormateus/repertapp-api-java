3ff3aab9a278f173ec9efb528a49003bbade4320
igormateus <igor_matteus@yahoo.com.br>
2022-02-03 20:03:38 -0300

    add not blank validations

--- src/main/java/io/github/igormateus/repertapp/dto/user/UserAuthDTO.java
+++ src/main/java/io/github/igormateus/repertapp/dto/user/UserAuthDTO.java
@@ -1,11 +1,15 @@
 package io.github.igormateus.repertapp.dto.user;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
 public class UserAuthDTO {
 
+    @NotBlank
     private String username;
-
+  
+    @NotBlank
     private String password;
 }
